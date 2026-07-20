1990a1c58140eac4d6b712cf17622d792b298286
Rhafael Freitas da Costa <rhafael.costa@lactec.com.br>
2026-07-20 20:45:31 -0300

    Dockerização do orquestrador dos microsserviços

---
+++ etc/postgres/init-user-db.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+	CREATE DATABASE fastpay;
+	CREATE DATABASE billing;
+	CREATE DATABASE ordering;
+	CREATE DATABASE billing_test;
+	CREATE DATABASE ordering_test;
+EOSQL
